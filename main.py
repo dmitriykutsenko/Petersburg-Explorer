bb44b817baa77bf9ed7ee209aad989b4bfcc54b9
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-04-04 22:31:34 +0300

    Добавил получение рандомизированных данных из БД и интеграцию их в html при отображении страницы

--- main.py
+++ main.py
@@ -5,44 +5,55 @@ from werkzeug.exceptions import abort
 from data.cluster import Cluster
 from data.panorama import Panorama
 from data.user import User
+import random
 
 from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'petersburg_explorer_secret_key'
 
+def get_panoramas_data():
+    db_sess = db_session.create_session()
+    clusters = db_sess.query(Cluster)
+
+    for cluster in clusters:
+        panoramas = cluster.panoramas.split()
+
+    panoramas = [int(elem) for elem in panoramas]
+
+    panoramas_dict = {}
+
+    for panorama_from_db in db_sess.query(Panorama):
+        panoramas_dict[panorama_from_db.name] = [panorama_from_db.x, panorama_from_db.y]
+
+    print(panoramas_dict)
+
+    i1, i2 = random.sample(range(len(panoramas_dict.keys())), 2)
+    print(i1, i2)
+    return panoramas_dict, i1, i2
 
 @app.route("/", methods=['POST', 'GET'])
-def panorama():
+def game_screen():
     if request.method == 'GET':
-        db_sess = db_session.create_session()
-        clusters = db_sess.query(Cluster)
+        panoramas_dict, ind1, ind2 = get_panoramas_data()
 
-        for cluster in clusters:
-            panoramas = cluster.panoramas.split()
+        print(list(panoramas_dict.keys()))
 
-        panoramas = [int(elem) for elem in panoramas]
-
-        panoramas_dict = {}
-
-        for panorama_from_db in db_sess.query(Panorama):
-            panoramas_dict[panorama_from_db.name] = [panorama_from_db.x, panorama_from_db.y]
-
-        print(panoramas_dict)
-        print('here')
-
-        # вызов js функции для перемещения панорамы в нужную точку
-        return render_template('panorama.html')
+        return render_template('panorama.html', destination=list(panoramas_dict.keys())[ind2],
+                               x=panoramas_dict[list(panoramas_dict.keys())[ind1]][0],
+                               y=panoramas_dict[list(panoramas_dict.keys())[ind1]][1])
 
     elif request.method == 'POST':
         # вызов js функции получения координат
         # переключение на следющую панораму при помощи js функции
-
-        return render_template('panorama.html')
+        panoramas_dict, ind1, ind2 = get_panoramas_data()
+        return render_template('panorama.html', destination=list(panoramas_dict.keys())[ind2],
+                               x=panoramas_dict[list(panoramas_dict.keys())[ind1]][0],
+                               y=panoramas_dict[list(panoramas_dict.keys())[ind1]][1])
 
 
 def main():
-    db_session.global_init('db/explorer.db')
+    db_session.global_init('db/Petersburg.db')
     app.run(port=8000)
 
 
