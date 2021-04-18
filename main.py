765e3efa72a5dc714e0e83cfb01de9e3405990da
kostenbl4 <75079998+kostenbl4@users.noreply.github.com>
2021-04-18 22:35:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def login():
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return redirect("/start")
+            return redirect("/")
 
         return render_template('login.html',
                                message="Неправильный логин или пароль",
