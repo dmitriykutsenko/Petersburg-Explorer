a4c03027d01034445fecb9e0aec046ead675fe5d
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-04-22 16:57:38 +0300

    сделал сохранение всех промежуточных данных в БД

--- data/game_session.py
+++ data/game_session.py
@@ -11,9 +11,14 @@ class GameSession(SqlAlchemyBase):
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
 
-    date = sqlalchemy.Column(sqlalchemy.Date, nullable=False)
+    round = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
 
-    finalScore = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    completed = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    destination_coordinates = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    date = sqlalchemy.Column(sqlalchemy.Date, nullable=True)
+
+    totalScore = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
 
     firstRound = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     secondRound = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
@@ -31,3 +36,12 @@ class GameSession(SqlAlchemyBase):
 
     def setFourthRoundScore(self, score):
         self.fourthRound = score
+
+    def setRound(self, round):
+        self.round = round
+
+    def setScore(self, score):
+        self.totalScore = score
+
+    def setDestinationCoordinates(self, coordinates):
+        self.destination_coordinates = coordinates
--- web_infrastructure/game_blueprint.py
+++ web_infrastructure/game_blueprint.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, redirect, session, request
+from flask import Blueprint, render_template, redirect, request
 
+from data import db_session
+from data.game_session import GameSession
 from score_scripts.get_panoramas_data import get_panoramas_data
 from score_scripts.parsers import parse_coordinates
 from score_scripts.parsers import parse_destination_coordinates
@@ -10,12 +12,14 @@ blueprint = Blueprint(__name__, 'game_blueprint', template_folder='templates')
 
 @blueprint.route("/")
 def index():
-    session['Round'] = 1
-    session['Score'] = 0
-    session['Current Destination Coordinates'] = -1
-    session['Current Coordinates'] = -1
-    session.permanent = False
-    session.modified = True
+    db_sess = db_session.create_session()
+
+    gameSession = GameSession()
+    gameSession.setRound(1)
+    gameSession.setScore(0)
+
+    db_sess.add(gameSession)
+    db_sess.commit()
 
     return render_template('start.html')
 
@@ -23,35 +27,66 @@ def index():
 @blueprint.route("/game/", methods=['POST', 'GET'])
 def game_screen():
     if request.method == 'GET':
-        panoramas_dict, ind1, ind2 = get_panoramas_data(session['Round'])
+        db_sess = db_session.create_session()
+        gameSession = db_sess.query(GameSession).all()[-1]
+
+        currentRound = gameSession.round
+
+        panoramas_dict, ind1, ind2 = get_panoramas_data(currentRound)
 
         current_start_coords = (panoramas_dict[list(panoramas_dict.keys())[ind1]][0],
                                 panoramas_dict[list(panoramas_dict.keys())[ind1]][1])
 
-        session['Current Destination Coordinates'] = [panoramas_dict[list(panoramas_dict.keys())[ind2]][0],
-                                                      panoramas_dict[list(panoramas_dict.keys())[ind2]][1]]
+        dest_coordinates = " ".join([str(panoramas_dict[list(panoramas_dict.keys())[ind2]][0]),
+                                     str(panoramas_dict[list(panoramas_dict.keys())[ind2]][1])])
 
-        session.modified = True
+        score = gameSession.totalScore
+
+        gameSession.setDestinationCoordinates(dest_coordinates)
+
+        db_sess.commit()
+
         return render_template('panorama.html',
                                destination=list(panoramas_dict.keys())[ind2],
                                x=current_start_coords[0],
                                y=current_start_coords[1],
-                               round=session['Round'], score=session['Score'])
+                               round=currentRound, score=score)
 
     elif request.method == 'POST':
         response = request.get_data().decode()[1:-1].replace('"x":', ""). \
             replace(',"y"', '').replace(".", "").split(":")
 
-        session['Round'] += 1
-        print("ROUND UPDATED")
+        db_sess = db_session.create_session()
+        gameSession = db_sess.query(GameSession).all()[-1]
 
-        session['Score'] += count_score(parse_coordinates(response),
-                                        parse_coordinates(parse_destination_coordinates(
-                                            session['Current Destination Coordinates'])))
+        currentRound = gameSession.round
+        currentRound += 1
+        gameSession.setRound(currentRound)
 
-        session.modified = True
+        print("ROUND UPDATED:", currentRound)
 
-        if session['Round'] == 5:
-            return render_template('endgame.html', score=session['Score'])
+        score = gameSession.totalScore
+
+        resultScore = count_score(parse_coordinates(response),
+                                  parse_coordinates(parse_destination_coordinates(
+                                      gameSession.destination_coordinates.split(" "))))
+
+        print("COUNTED SCORE WITH START:", response, "AND", gameSession.destination_coordinates)
+        print(resultScore, score)
+
+        score += resultScore
+
+        gameSession.setScore(score)
+
+        db_sess.commit()
+
+        if str(currentRound) == "5":
+            print("AAA")
+            return redirect('/finish_game/')
 
         return redirect('/game/')
+
+
+@blueprint.route('/finish_game/')
+def finish():
+    return render_template('endgame.html', score=228)
