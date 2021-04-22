e61b5c3606bc192d251d508d9a5ebbca5a832ded
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-04-23 01:03:24 +0300

    сделал подсчет в js, почти сделал сохранение переменной

--- web_infrastructure/game_blueprint.py
+++ web_infrastructure/game_blueprint.py
@@ -4,8 +4,6 @@ from data import db_session
 from data.game_session import GameSession
 from score_scripts.get_panoramas_data import get_panoramas_data
 from score_scripts.parsers import parse_coordinates
-from score_scripts.parsers import parse_destination_coordinates
-from score_scripts.score_count import count_score
 
 blueprint = Blueprint(__name__, 'game_blueprint', template_folder='templates')
 
@@ -37,29 +35,22 @@ def game_screen():
         current_start_coords = (panoramas_dict[list(panoramas_dict.keys())[ind1]][0],
                                 panoramas_dict[list(panoramas_dict.keys())[ind1]][1])
 
-        dest_coordinates = " ".join([str(panoramas_dict[list(panoramas_dict.keys())[ind2]][0]),
-                                     str(panoramas_dict[list(panoramas_dict.keys())[ind2]][1])])
+        dest_coordinates = parse_coordinates([str(panoramas_dict[list(panoramas_dict.keys())[ind2]][0]).replace(".", ""),
+                            str(panoramas_dict[list(panoramas_dict.keys())[ind2]][1]).replace(".", "")])
 
         score = gameSession.totalScore
 
-        gameSession.setDestinationCoordinates(dest_coordinates)
-
         db_sess.commit()
+
+        print(dest_coordinates)
 
         return render_template('panorama.html',
                                destination=list(panoramas_dict.keys())[ind2],
                                x=current_start_coords[0],
                                y=current_start_coords[1],
-                               round=currentRound, score=score)
-
-    elif request.method == 'PUT':
-        response = request.get_data().decode()[1:-1].replace('"x":', ""). \
-            replace(',"y"', '').replace(".", "").split(":")
-        db_sess = db_session.create_session()
-        gameSession = db_sess.query(GameSession).all()[-1]
-        gameSession.setCurrentCoordinates(" ".join(response))
-        db_sess.commit()
-        return 'caught coordinates'
+                               round=currentRound,
+                               xCount=dest_coordinates[0],
+                               yCount=dest_coordinates[1])
 
     elif request.method == "POST":
         db_sess = db_session.create_session()
@@ -70,15 +61,6 @@ def game_screen():
         gameSession.setRound(currentRound)
 
         print("ROUND UPDATED:", currentRound)
-
-        resultScore = count_score(parse_coordinates(gameSession.current_coordinates.split()),
-                                  parse_coordinates(parse_destination_coordinates(
-                                      gameSession.destination_coordinates.split())))
-
-        score = gameSession.totalScore
-        score += resultScore
-
-        gameSession.setScore(score)
 
         db_sess.commit()
 
