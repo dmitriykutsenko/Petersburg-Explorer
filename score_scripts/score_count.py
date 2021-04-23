a183f5544aaff8d2593704981005a6849147e66f
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-04-23 23:24:49 +0300

    увеличил количество выдаваемых очков

--- score_scripts/score_count.py
+++ score_scripts/score_count.py
@@ -4,7 +4,7 @@ import math
 def count_score(curr_coords, dest_coords):
     distance = math.sqrt((dest_coords[0] - curr_coords[0]) ** 2 + (dest_coords[1] - curr_coords[1]) ** 2)
     print(distance)
-    score = 10 / distance * 100000
+    score = 10 / distance * 200000
     if score > 1000:
         score = 1000
     return int(score)
