1bde0c9797f20b32ec34175a9152fb7547d9982d
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-04-23 23:53:17 +0300

    commit

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.register_blueprint(game_blueprint.blueprint)
 def main():
     db_session.global_init('db/Petersburg.db')
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='0.0.0.0', port=port)
     # app.run(host='127.0.0.1', port=8080)
 
 
