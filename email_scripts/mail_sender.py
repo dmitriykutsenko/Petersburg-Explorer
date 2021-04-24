6b0b1a71ee87af9bf23744b418f07a63ee50af64
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-04-25 01:57:33 +0300

    вернул рассылку писем с кодами подтверждения при регистрации

--- email_scripts/mail_sender.py
+++ email_scripts/mail_sender.py
@@ -1,25 +1,29 @@
-import smtplib
 import os
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
 def send_email(email, subject, text):
-    addr_from = os.getenv("FROM")
-    password = os.getenv("PASSWORD")
+    try:
+        addr_from = os.getenv("FROM")
+        password = os.getenv("PASSWORD")
 
-    msg = MIMEMultipart()
-    msg['From'] = addr_from
-    msg['To'] = email
-    msg['Subject'] = subject
+        msg = MIMEMultipart()
+        msg['From'] = addr_from
+        msg['To'] = email
+        msg['Subject'] = subject
 
-    body = text
-    msg.attach(MIMEText(body, 'plain'))
+        body = text
+        msg.attach(MIMEText(body, 'plain'))
 
-    server = smtplib.SMTP_SSL(os.getenv('HOST'), os.getenv('PORT'))
-    server.login(addr_from, password)
+        server = smtplib.SMTP_SSL(os.getenv('HOST'), os.getenv('PORT'))
+        server.login(addr_from, password)
 
-    server.send_message(msg)
-    server.quit()
+        server.send_message(msg)
+        server.quit()
 
-    return True
+        return True
+
+    except Exception:
+        return False
