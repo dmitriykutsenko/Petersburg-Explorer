d7cfd2300e827277c9c7a86a53ee0a0d47b5ba0a
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-04-25 19:02:08 +0300

    commit

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
     db_session.global_init('db/Petersburg.db')
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-    # app.run(host='127.0.0.1', port=7000)
+    # app.run(host='127.0.0.1', port=1488)
 
 
 if __name__ == '__main__':
