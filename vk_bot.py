186dd0f3c7c1960a0d8d2ed5582b10bf708f6879
zelakk <v.kalskov@mail.ru>
2021-04-25 23:48:57 +0300

    окончательный бот

--- vk_bot.py
+++ vk_bot.py
@@ -2,12 +2,14 @@ import string
 
 import vk
 import vk_api
-from vk_api import keyboard
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 import datetime
+from pyowm import OWM
+from pyowm.utils.config import get_default_config
 
 TOKEN = '9a91352c9040eb78f534e8b0d69cb6c3409aabc434dce6e3fe4283c8f5ff08b7c364c766e22fc0fd157b8'
+API = 'efe788c9ea7384d848c36fe48753fa66'
 
 bad_words = ['какашка', 'тупой', 'плохой', 'ужасный', 'нехороший', 'соси', 'салют', 'дибил']
 hello_words = ['привет', 'хай', 'здарова']
@@ -36,7 +38,8 @@ def bot():
                                  message=f"1. Сайт \n"
                                          f"2. Информация\n"
                                          f"3. Время \n"
-                                         f"4. Дата\n"
+                                         f"4. Дата \n"
+                                         f"5. Погода \n"
                                          f"Также вы можете немного поговорить с ним, используя обычные фразы.",
                                  random_id=random.randint(0, 2 ** 64))
             elif event.obj.message['text'].lower().rstrip(string.punctuation).strip() == 'сайт':
@@ -48,7 +51,7 @@ def bot():
                                  random_id=random.randint(0, 2 ** 64))
             elif event.obj.message['text'].lower().rstrip(string.punctuation).strip() == 'информация':
                 vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"Petersburg Explorer - это новая игра о Санкт-Петербурге. Вы погружаетесь в северную столицу России благодаря панорам Яндекс карт. \n \n"
+                                 message=f"Petersburg Explorer - это новая игра о Санкт-Петербурге. Вы погружаетесь в северную столицу России благодаря панорамам Яндекс карт. \n \n"
                                             "В процессе игры вы будете гулять по городу. Вам нужно будет дойти до определённого места. "
                                             "Чем ближе вы придёте к месту назначения, тем больше очков вы получите! Так что вперёд гулять по нашему любимому городу!",
                                  random_id=random.randint(0, 2 ** 64))
@@ -59,6 +62,17 @@ def bot():
             elif event.obj.message['text'].lower().rstrip(string.punctuation) == 'время':
                 vk.messages.send(user_id=event.obj.message['from_id'],
                                  message=now.strftime("%H:%M:%S"),
+                                 random_id=random.randint(0, 2 ** 64))
+            elif event.obj.message['text'].lower().rstrip(string.punctuation) == 'погода':
+                config_dict = get_default_config()
+                config_dict['language'] = 'ru'
+                owm = OWM(API, config_dict)
+                manager = owm.weather_manager()
+                observation = manager.weather_at_place('Санкт-Петербург')
+                pogoda = observation.weather
+                temperature = pogoda.temperature('celsius')['temp']
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message=f"Сейчас в городе Санкт-Петербурге " + str(int(temperature)) + " °С",
                                  random_id=random.randint(0, 2 ** 64))
             elif event.obj.message['text'].lower().rstrip(string.punctuation).strip() in hello_words:
                 vk.messages.send(user_id=event.obj.message['from_id'],
@@ -84,5 +98,7 @@ def bot():
                 vk.messages.send(user_id=event.obj.message['from_id'],
                                  message=f"{event.obj.message['text'].capitalize().rstrip(string.punctuation)}? Вы уверены? ",
                                  random_id=random.randint(0, 2 ** 64))
+
+
 if __name__ == '__main__':
     bot()
