5759919413f7c57b58998ceb41bfbaeaac70a1f7
Dmitry Kutsenko <dmitrykutsenko@MacBook-Pro-Dmitry.local>
2023-05-07 00:28:25 +0300

    Added exception handlers to the blueprints Got rid of "Try-Except" wrappings for every block of code, added exception handlers instead

--- web_infrastructure/game_blueprint.py
+++ web_infrastructure/game_blueprint.py
@@ -13,166 +13,147 @@ from score_scripts.parsers import to_int_parser
 from score_scripts.score_count import count_score
 
 blueprint = Blueprint('game_blueprint', __name__, template_folder='templates')
+
+
 # logging.basicConfig(level=logging.INFO, filename='gamesession.log',
 #                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+@blueprint.errorhandler(Exception)
+def handle_error(e):
+    return render_template('error.html'), 500
 
 
 @blueprint.route("/")
 def index():
-    try:
-        db_sess = db_session.create_session()
+    db_sess = db_session.create_session()
 
-        game_session = GameSession()
+    game_session = GameSession()
 
-        db_sess.add(game_session)
-        db_sess.commit()
+    db_sess.add(game_session)
+    db_sess.commit()
 
-        db_sess = db_session.create_session()
-        game_session = db_sess.query(GameSession).all()[-1]
-        session['sessionId'] = game_session.id
-        logging.info("STARTED A NEW GAMESESSION (id = {})".format(str(session['sessionId'])))
+    db_sess = db_session.create_session()
+    game_session = db_sess.query(GameSession).all()[-1]
+    session['sessionId'] = game_session.id
+    logging.info("STARTED A NEW GAMESESSION (id = {})".format(str(session['sessionId'])))
 
-        return render_template('start.html')
-
-    except Exception:
-        return render_template('error.html')
+    return render_template('start.html')
 
 
 @blueprint.route("/game/", methods=['POST', 'GET', 'PUT'])
 @login_required
 def game_screen():
     if request.method == 'GET':
-        try:
-            db_sess = db_session.create_session()
-            game_session = db_sess.query(GameSession).filter(GameSession.id == session['sessionId']).first()
+        db_sess = db_session.create_session()
+        game_session = db_sess.query(GameSession).filter(GameSession.id == session['sessionId']).first()
 
-            current_round = game_session.round
-            if str(current_round) == '5':
-                logging.info("REDIRECTED GAMESESSION (id = {}) TO FINISH SCREEN".format(str(session['sessionId'])))
-                return redirect('/finish_game/')
+        current_round = game_session.round
+        if str(current_round) == '5':
+            logging.info("REDIRECTED GAMESESSION (id = {}) TO FINISH SCREEN".format(str(session['sessionId'])))
+            return redirect('/finish_game/')
 
-            if int(current_round) > 5:
-                logging.fatal("GAMESESSION's ROUND NUMBER BECAME MORE THAN 5")
-                return render_template('error.html')
+        if int(current_round) > 5:
+            logging.fatal("GAMESESSION's ROUND NUMBER BECAME MORE THAN 5")
+            return render_template('error.html')
 
-            cluster_id = random.randint(1, 7)
+        cluster_id = random.randint(1, 7)
 
-            panoramas_dict, ind1, ind2 = get_panoramas_data(str(cluster_id))
+        panoramas_dict, ind1, ind2 = get_panoramas_data(str(cluster_id))
 
-            start_coordinates = (panoramas_dict[list(panoramas_dict.keys())[ind1]][0],
-                                 panoramas_dict[list(panoramas_dict.keys())[ind1]][1])
+        start_coordinates = (panoramas_dict[list(panoramas_dict.keys())[ind1]][0],
+                             panoramas_dict[list(panoramas_dict.keys())[ind1]][1])
 
-            db_dest_coordinates = game_session.destination_coordinates_list.split(";")
+        db_dest_coordinates = game_session.destination_coordinates_list.split(";")
 
-            dest_coordinates = " ".join(
-                parse_coordinates(
-                    parse_destination_coordinates(
-                        [str(panoramas_dict[list(panoramas_dict.keys())[ind2]][0]),
+        dest_coordinates = " ".join(
+            parse_coordinates(
+                parse_destination_coordinates(
+                    [str(panoramas_dict[list(panoramas_dict.keys())[ind2]][0]),
 
-                         str(panoramas_dict[list(panoramas_dict.keys())[ind2]][1])]
-                    )
+                     str(panoramas_dict[list(panoramas_dict.keys())[ind2]][1])]
                 )
             )
+        )
 
-            db_dest_coordinates.append(dest_coordinates)
+        db_dest_coordinates.append(dest_coordinates)
 
-            game_session.set_destination_coordinates(";".join(db_dest_coordinates))
+        game_session.set_destination_coordinates(";".join(db_dest_coordinates))
 
-            db_sess.commit()
+        db_sess.commit()
 
-            logging.info("NEW DESTINATION COORDINATES WERE PUT CORRECTLY: {} {}".format(db_dest_coordinates[0],
-                                                                                        db_dest_coordinates[1]))
+        logging.info("NEW DESTINATION COORDINATES WERE PUT CORRECTLY: {} {}".format(db_dest_coordinates[0],
+                                                                                    db_dest_coordinates[1]))
 
-            return render_template('panorama.html',
-                                   destination=list(panoramas_dict.keys())[ind2],
-                                   x=start_coordinates[0],
-                                   y=start_coordinates[1],
-                                   round=current_round)
-
-        except Exception:
-            logging.fatal('ERROR OCCURED IN GET HANDLER')
-            return render_template('error.html')
+        return render_template('panorama.html',
+                               destination=list(panoramas_dict.keys())[ind2],
+                               x=start_coordinates[0],
+                               y=start_coordinates[1],
+                               round=current_round)
 
     elif request.method == 'PUT':
-        try:
-            response = request.get_data().decode()[1:-1].replace('"x":', ""). \
-                replace(',"y"', '').replace(".", "").split(":")
-            db_sess = db_session.create_session()
-            game_session = db_sess.query(GameSession).filter(GameSession.id == session['sessionId']).first()
+        response = request.get_data().decode()[1:-1].replace('"x":', ""). \
+            replace(',"y"', '').replace(".", "").split(":")
+        db_sess = db_session.create_session()
+        game_session = db_sess.query(GameSession).filter(GameSession.id == session['sessionId']).first()
 
-            db_finish_coordinates = game_session.finish_coordinates_list.split(";")
-            db_finish_coordinates.append(
-                " ".join(
-                    parse_coordinates(response)
-                )
+        db_finish_coordinates = game_session.finish_coordinates_list.split(";")
+        db_finish_coordinates.append(
+            " ".join(
+                parse_coordinates(response)
             )
+        )
 
-            game_session.set_finish_coordinates(";".join(db_finish_coordinates))
-            db_sess.commit()
-            logging.info("NEW DESTINATION COORDINATES WERE PUT CORRECTLY: {} {}".format(db_finish_coordinates[0],
-                                                                                        db_finish_coordinates[1]))
-            return 'caught coordinates'
-
-        except Exception:
-            logging.fatal('ERROR OCCURED IN PUT HANDLER')
-            return render_template('error.html')
+        game_session.set_finish_coordinates(";".join(db_finish_coordinates))
+        db_sess.commit()
+        logging.info("NEW DESTINATION COORDINATES WERE PUT CORRECTLY: {} {}".format(db_finish_coordinates[0],
+                                                                                    db_finish_coordinates[1]))
+        return 'caught coordinates'
 
     elif request.method == "POST":
-        try:
-            db_sess = db_session.create_session()
-            game_session = db_sess.query(GameSession).filter(GameSession.id == session['sessionId']).first()
+        db_sess = db_session.create_session()
+        game_session = db_sess.query(GameSession).filter(GameSession.id == session['sessionId']).first()
 
-            current_round = game_session.round
-            current_round += 1
-            game_session.set_round(current_round)
+        current_round = game_session.round
+        current_round += 1
+        game_session.set_round(current_round)
 
-            logging.info("GAMESESSIONS'S (id = {}) ROUND UPDATED TO {}".format(str(session['sessionId']),
-                                                                               str(current_round)))
+        logging.info("GAMESESSIONS'S (id = {}) ROUND UPDATED TO {}".format(str(session['sessionId']),
+                                                                           str(current_round)))
 
-            db_sess.commit()
+        db_sess.commit()
 
-            return redirect('/game/')
-
-        except Exception:
-            logging.fatal('ERROR OCCURED IN POST HANDLER')
-            return render_template('error.html')
+        return redirect('/game/')
 
 
 @blueprint.route('/finish_game/')
 @login_required
 def finish():
-    try:
-        db_sess = db_session.create_session()
-        db_sess.expire_on_commit = False
-        game_session = db_sess.query(GameSession).filter(GameSession.id == session['sessionId']).first()
+    db_sess = db_session.create_session()
+    db_sess.expire_on_commit = False
+    game_session = db_sess.query(GameSession).filter(GameSession.id == session['sessionId']).first()
 
-        total_score = 0
+    total_score = 0
 
-        finish_coordinates = game_session.finish_coordinates_list.split(";")
-        destination_coordinates = game_session.destination_coordinates_list.split(";")
+    finish_coordinates = game_session.finish_coordinates_list.split(";")
+    destination_coordinates = game_session.destination_coordinates_list.split(";")
 
-        for i in range(1, 5):
-            this_finish_coordinates = finish_coordinates[i]
-            this_destination_coordinates = destination_coordinates[i]
+    for i in range(1, 5):
+        this_finish_coordinates = finish_coordinates[i]
+        this_destination_coordinates = destination_coordinates[i]
 
-            plus_score = count_score(to_int_parser(this_finish_coordinates.split()),
-                                     to_int_parser(this_destination_coordinates.split()))
-            game_session.set_round_score(i, plus_score)
-            total_score += plus_score
+        plus_score = count_score(to_int_parser(this_finish_coordinates.split()),
+                                 to_int_parser(this_destination_coordinates.split()))
+        game_session.set_round_score(i, plus_score)
+        total_score += plus_score
 
-        game_session.set_user_id(current_user.id)
-        game_session.set_score(total_score)
+    game_session.set_user_id(current_user.id)
+    game_session.set_score(total_score)
 
-        db_sess.commit()
+    db_sess.commit()
 
-        logging.info("GAMESESSION (id = {}) FINISHED CORRECTLY".format(str(session['sessionId'])))
+    logging.info("GAMESESSION (id = {}) FINISHED CORRECTLY".format(str(session['sessionId'])))
 
-        return render_template('endgame.html', score=total_score)
-
-    except Exception:
-        logging.fatal('ERROR OCCURED DURING COUNTING TOTAL SCORE IN GAMESESSION (id = {})'
-                      .format(str(session['sessionId'])))
-        return render_template('error.html')
+    return render_template('endgame.html', score=total_score)
 
 
 @blueprint.route('/info/')
--- web_infrastructure/users_blueprint.py
+++ web_infrastructure/users_blueprint.py
@@ -19,200 +19,154 @@ from forms.search import SearchForm
 blueprint = Blueprint('users_blueprint', __name__, template_folder='templates')
 
 
+@blueprint.errorhandler(Exception)
+def handle_error(e):
+    return render_template('error.html'), 500
+
+
 @blueprint.route("/signup", methods=['GET', 'POST'])
 def register():
-    try:
-        form = RegisterForm()
-        if form.validate_on_submit():
-            if len(form.name.data) < 3:
-                return render_template('register.html', title='Регистрация',
-                                       form=form,
-                                       message=f"Слишком короткое имя пользователя ({len(form.name.data)} < 3)")
-            if len(form.name.data) > 9:
-                return render_template('register.html', title='Регистрация',
-                                       form=form,
-                                       message=f"Слишком длиное имя пользователя ({len(form.name.data)} > 9)")
-            if form.password.data != form.password_again.data:
-                return render_template('register.html', title='Регистрация',
-                                       form=form,
-                                       message="Пароли не совпадают")
-            db_sess = db_session.create_session()
-            if db_sess.query(User).filter(User.email == form.email.data).first():
-                return render_template('register.html', title='Регистрация',
-                                       form=form,
-                                       message="Такой пользователь уже есть")
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if len(form.name.data) < 3:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message=f"Слишком короткое имя пользователя ({len(form.name.data)} < 3)")
+        if len(form.name.data) > 9:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message=f"Слишком длиное имя пользователя ({len(form.name.data)} > 9)")
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
 
-            verification_code = generate_code()
+        verification_code = generate_code()
 
-            session['Verification Code'] = verification_code
-            session['User Email'] = form.email.data
-            session['User Nickname'] = form.name.data
-            session['User Password'] = form.password.data
+        session['Verification Code'] = verification_code
+        session['User Email'] = form.email.data
+        session['User Nickname'] = form.name.data
+        session['User Password'] = form.password.data
 
-            if send_email(session['User Email'], verification_code):
-                # logging.info('EMAIL LETTER WAS SENT. REDIRECTED TO EMAIL VERIFICATION HANDLER')
-                return redirect('/email_verification')
+        if send_email(session['User Email'], verification_code):
+            # logging.info('EMAIL LETTER WAS SENT. REDIRECTED TO EMAIL VERIFICATION HANDLER')
+            return redirect('/email_verification')
 
-            else:
-                return render_template('register.html',
-                                       title='Регистрация',
-                                       form=form,
-                                       message="К сожалению, письмо не было отправлено. Попробуйте еще раз")
+        else:
+            return render_template('register.html',
+                                   title='Регистрация',
+                                   form=form,
+                                   message="К сожалению, письмо не было отправлено. Попробуйте еще раз")
 
-        return render_template('register.html', title='Регистрация', form=form)
-
-    except Exception:
-        return render_template('error.html')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 @blueprint.route('/email_verification', methods=['GET', 'POST'])
 def email_verification():
-    try:
-        form = EmailVerificationForm()
-        if form.validate_on_submit():
-            db_sess = db_session.create_session()
-            if session['Verification Code'] == form.code.data:
-                user = User(
-                    name=session['User Nickname'],
-                    lower_name=session['User Nickname'].lower(),
-                    email=session['User Email']
-                )
-                user.set_password(session['User Password'])
-                db_sess.add(user)
-                db_sess.commit()
+    form = EmailVerificationForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        if session['Verification Code'] == form.code.data:
+            user = User(
+                name=session['User Nickname'],
+                lower_name=session['User Nickname'].lower(),
+                email=session['User Email']
+            )
+            user.set_password(session['User Password'])
+            db_sess.add(user)
+            db_sess.commit()
 
-                logging.info('ADDED A NEW USER: name={}, email={}'.format(
-                    user.name, user.email))
+            logging.info('ADDED A NEW USER: name={}, email={}'.format(
+                user.name, user.email))
 
-                return redirect('/login')
+            return redirect('/login')
 
-            else:
-                return render_template('email_verification.html', title='Подтверждение', form=form,
-                                       message='Неверный код подтверждения')
+        else:
+            return render_template('email_verification.html', title='Подтверждение', form=form,
+                                   message='Неверный код подтверждения')
 
-        return render_template('email_verification.html', title='Подтверждение', form=form)
-
-    except Exception:
-        logging.fatal("ERROR OCCURED DURING VERIFICATING USER'S EMAIL")
-        return render_template('error.html')
+    return render_template('email_verification.html', title='Подтверждение', form=form)
 
 
 @blueprint.route('/login', methods=['GET', 'POST'])
 def login():
-    try:
-        form = LoginForm()
-        if form.validate_on_submit():
-            db_sess = db_session.create_session()
-            user = db_sess.query(User).filter(
-                User.email == form.email.data).first()
-            if user and user.check_password(form.password.data):
-                login_user(user, remember=form.remember_me.data)
-                return redirect("/")
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(
+            User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
 
-            return render_template('login.html',
-                                   message="Неправильный логин или пароль",
-                                   form=form)
-        return render_template('login.html', title='Авторизация', form=form)
-
-    except Exception:
-        logging.fatal('ERROR OCCURED DURING LOGINING')
-        return render_template('error.html')
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @blueprint.route('/profile')
 @login_required
 def profile():
-    try:
-        return redirect('/users/{}'.format(current_user.name))
-
-    except Exception as e:
-        print(e)
-        logging.fatal("ERROR OCCURED DURINGG SHOWING USER'S (id = {}) PROFILE".format(
-            current_user.id))
-        return render_template('error.html')
+    return redirect('/users/{}'.format(current_user.name))
 
 
 @blueprint.route('/delete_history')
 @login_required
 def delete_history():
-    try:
-        db_sess = db_session.create_session()
+    db_sess = db_session.create_session()
 
-        game_sessions = db_sess.query(GameSession).filter(
-            (GameSession.user_id == current_user.id))
-        for sess in game_sessions:
-            db_sess.delete(sess)
-        db_sess.commit()
-        return redirect('/profile')
-
-    except Exception:
-        logging.fatal("ERROR OCCURED DURINGG SHOWING USER'S (id = {}) PROFILE".format(
-            current_user.id))
-        return render_template('error.html')
+    game_sessions = db_sess.query(GameSession).filter(
+        (GameSession.user_id == current_user.id))
+    for sess in game_sessions:
+        db_sess.delete(sess)
+    db_sess.commit()
+    return redirect('/profile')
 
 
 @blueprint.route('/logout')
 @login_required
 def logout():
-    try:
-        logout_user()
-        return redirect("/")
-
-    except Exception:
-        return render_template('error.html')
+    logout_user()
+    return redirect("/")
 
 
 @blueprint.route('/users/<username>')
 def view_user(username):
-    try:
-        db_sess = db_session.create_session()
+    db_sess = db_session.create_session()
 
-        user = db_sess.query(User).filter(
-            (User.name == username)).first()
-        game_sessions = db_sess.query(GameSession).filter(
-            (GameSession.user_id == user.id))
+    user = db_sess.query(User).filter(
+        (User.name == username)).first()
+    game_sessions = db_sess.query(GameSession).filter(
+        (GameSession.user_id == user.id))
 
-        return render_template("profile.html", game_sessions=reversed(list(game_sessions)), user=user)
-
-    except Exception as e:
-        print(e)
-        logging.fatal("ERROR OCCURED DURINGG SHOWING USER'S (id = {}) PROFILE".format(
-            current_user.id))
-        return render_template('error.html')
+    return render_template("profile.html", game_sessions=reversed(list(game_sessions)), user=user)
 
 
 @blueprint.route('/search', methods=['POST'])
 def search():
-    try:
-        form = SearchForm()
+    form = SearchForm()
 
-        if form.validate_on_submit:
-            searched = form.searched.data
+    if form.validate_on_submit:
+        searched = form.searched.data
 
-            return redirect('/search/{}'.format(searched))
-        else:
-            pass
-
-    except Exception as e:
-        print(e)
-        logging.fatal("ERROR OCCURED DURINGG SEARCHING USER'S (NAME = {}) PROFILE".format(
-            searched))
-        return render_template('error.html')
+        return redirect('/search/{}'.format(searched))
+    else:
+        pass
 
 
 @blueprint.route('/search/<searched>', methods=['GET'])
 def view_searched(searched):
-    try:
-        if searched:
-            db_sess = db_session.create_session()
-            users = db_sess.query(User).filter(
-                User.lower_name.like('%' + searched.lower() + '%'))
-            users = users.order_by(User.name).all()
-        else:
-            users = []
-        return render_template('search.html', searched=searched, users=users)
-    except Exception as e:
-        print(e)
-        logging.fatal("ERROR OCCURED DURINGG SEARCHING USER'S (NAME = {}) PROFILE".format(
-            searched))
-        return render_template('error.html')
+    if searched:
+        db_sess = db_session.create_session()
+        users = db_sess.query(User).filter(
+            User.lower_name.like('%' + searched.lower() + '%'))
+        users = users.order_by(User.name).all()
+    else:
+        users = []
+    return render_template('search.html', searched=searched, users=users)
